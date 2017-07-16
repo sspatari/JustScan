95c5854a645f1a4fc9cc2b29121b6057343540d1
Sasha <vdovicenco.alexandr@gmail.com>
2017-07-16 21:21:10 +0300

    Created MVP which includes image recognition and info searching

--- index.js
+++ index.js
@@ -2,30 +2,10 @@ const express = require('express'),
       app = express(),
       config = require('./config'),
       fs = require('fs'),
-      PDFParser = require("pdf2json")
-      extratInfo = require("./extractInfo"),
       uploadImage = require("./uploadImage")
 
-let pdfParser = new PDFParser(this,1)
 
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-pdfParser.on("pdfParser_dataReady", pdfData => {
-  data = pdfParser.getRawTextContent()
-  // fs.writeFile("./test.txt", pdfParser.getRawTextContent())
-  //
-  // fs.readFile('./test.txt', 'utf8', function (err,data) {
-  //   if (err) {
-  //     return console.log(err)
-  //   }
-  //   console.log("Succesufully read from text.txt")
-  //   createFileData(data, 151)
-  // })
-  textInfo = extratInfo.getNeededInfo(data, 10, 1)
-  console.log(textInfo)
-  extratInfo.createResultFile(textInfo)
-})
 
-// pdfParser.loadPDF("./pdfFolder/lex.justice.pdf")
 
 app.use('/api/v1/upload', uploadImage)
 app.get('/api/v1', (req, res)=> {
